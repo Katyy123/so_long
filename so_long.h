d7df632435dee905d760153579bcf1e3f9967d98
Catia Filiberto <cfiliber@e3r5p1.42roma.it>
2021-11-20 18:38:38 +0100

    20_11

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: cfiliber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 17:01:40 by cfiliber          #+#    #+#             */
-/*   Updated: 2021/11/19 15:04:11 by cfiliber         ###   ########.fr       */
+/*   Updated: 2021/11/20 13:01:40 by cfiliber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define SO_LONG_H
 
 #include <mlx.h>
+#include <stdio.h>
 
 typedef struct	s_data {
 	void	*img;
@@ -22,6 +23,11 @@ typedef struct	s_data {
 	int		line_length;
 	int		endian;
 }				t_data;
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
 
 int main(void);
 
